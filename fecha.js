343b3a1dea3527f8d4190afe401daa8eaab10bf8
Alejandro Barrera <alejandro91dark@hotmail.es>
2022-10-23 20:03:06 -0300

    clase03 concluida

--- fecha.js
+++ fecha.js
@@ -3,25 +3,25 @@ const data = new Date();
 
 // Creamos objeto para definir formato de fecha
 const dataOptions = {
-    weekend: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+  weekend: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
 }
 
 // Creamos objeto para definir formato de hora
 const hourOptions = {
-    hour12: false,
-    hour: 'numeric',
-    minute: '2-digit',
-    second: '2-digit',
-    timeZone: 'America/Sao_Paulo'
+  hour12: false,
+  hour: 'numeric',
+  minute: '2-digit',
+  second: '2-digit',
+  timeZone: 'America/Sao_Paulo'
 }
 
 // En caso de que necesitemos utilizar el formato en varios lugares del codigo
 const formatData = new Intl.DateTimeFormat('es-MX', {
-    ...dataOptions,
-    ...hourOptions
+  ...dataOptions,
+  ...hourOptions
 })
 
 
@@ -33,8 +33,8 @@ console.log(data.toLocaleTimeString('es-MX'));
 console.log(data.toLocaleTimeString('es-MX', hourOptions));
 
 console.log(data.toLocaleString('es-MX', {
-    ...dataOptions,
-    ...hourOptions
+  ...dataOptions,
+  ...hourOptions
 }));
 
 console.log(formatData.format(data));
